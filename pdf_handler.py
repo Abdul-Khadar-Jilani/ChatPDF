23ea9a2f67a1efc72b4d4caa87e02783c3579ade
Abdul Khadar Jilani <jilaniabdulgec@gmail.com>
2024-08-25 15:34:23 +0530

    Delete pdf_handler.py

--- pdf_handler.py
+++
@@ -1,8 +0,0 @@
-import PyPDF2
-
-def extract_text_from_pdf(pdf_file):
-    reader = PyPDF2.PdfReader(pdf_file)
-    text = ""
-    for page in range(len(reader.pages)):
-        text += reader.pages[page].extract_text()
-    return text
