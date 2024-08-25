d85cd10181c1ceed5b31c146a8204a7aaa156df1
Abdul Khadar Jilani <jilaniabdulgec@gmail.com>
2024-08-25 15:58:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from PyPDF2 import PdfReader
-from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain_community.llms import OpenAI
+from langchain_openai import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
 
 # Function to process the PDF
